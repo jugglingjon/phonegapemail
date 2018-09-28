bc4dab33945f473256ec54d89fd51719fa6b2c7b
Jonathan Cronin <cronin_jonathan@bah.com>
2018-09-28 11:07:05 -0400

    corrects filename

--- js/main.js
+++ js/main.js
@@ -30,28 +30,28 @@ $(document).ready(function(){
 	$('#download').click(function(){
 		//var base64 =  $('#output').val();
 		var txt=`<?xml version="1.0" encoding="utf-8"?>
-			<course_completion>
-			<Course_Completion_ID>e395c897-0cc5-4196-b54f-8e0d0f8cc18b</Course_Completion_ID>
-			<DODID>1234567890</DODID>
-			<Course_Number>CPPD-GMT-DV-1.1</Course_Number>
-			<Pre_Test_Score>0</Pre_Test_Score>
-			<Post_Test_Score>86.66666</Post_Test_Score>
-			<Completion_Date>2018-09-17T11:33:04.9427300-04:00</Completion_Date>
-			<Device_Information>
-			<Device_Manufacturer>Apple</Device_Manufacturer>
-			<Device_Model>iPhone</Device_Model>
-			<Device_Type>Phone</Device_Type>
-			<Device_OS_Name>iOS</Device_OS_Name>
-			<Device_OS_Version>11.1</Device_OS_Version>
-			</Device_Information>
-			</course_completion>`;
+<course_completion>
+	<Course_Completion_ID>e395c897-0cc5-4196-b54f-8e0d0f8cc18b</Course_Completion_ID>
+	<DODID>1234567890</DODID>
+	<Course_Number>CPPD-GMT-DV-1.1</Course_Number>
+	<Pre_Test_Score>0</Pre_Test_Score>
+	<Post_Test_Score>86.66666</Post_Test_Score>
+	<Completion_Date>2018-09-17T11:33:04.9427300-04:00</Completion_Date>
+	<Device_Information>
+		<Device_Manufacturer>Apple</Device_Manufacturer>
+		<Device_Model>iPhone</Device_Model>
+		<Device_Type>Phone</Device_Type>
+		<Device_OS_Name>iOS</Device_OS_Name>
+		<Device_OS_Version>11.1</Device_OS_Version>
+	</Device_Information>
+</course_completion>`;
 		var base64 = Base64.encode(txt);
 		var blobx = b64toBlob(base64, 'text/xml');
 		//location.href="data:text/xml;base64," + base64;
 		//saveAs(blobx,'test.xml');
 
 		var zip = new JSZip();
-		zip.file('text.xml',blobx);
+		zip.file('test.xml',blobx);
 
 		zip.generateAsync({type:"blob"}).then(function (blob) {
 			var reader = new FileReader();
